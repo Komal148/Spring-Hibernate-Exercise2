2b1267e2434abdf992726bf4323569af51eee5e7
Komal_R <komalr984@gmail.com>
2019-01-21 09:10:43 +0530

    Exercise-2Commit

--- src/main/java/com/stackroute/hibernate/config/ApplicationContextConfig.java
+++ src/main/java/com/stackroute/hibernate/config/ApplicationContextConfig.java
@@ -19,44 +19,44 @@ import com.stackroute.hibernate.model.Track;
 @EnableTransactionManagement
 public class ApplicationContextConfig {
 
-	@Bean
-	@Autowired
-	public DataSource getDataSource() {
+    @Bean
+    @Autowired
+    public DataSource getDataSource() {
 
-		BasicDataSource dataSource = new BasicDataSource();
+        BasicDataSource dataSource = new BasicDataSource();
 
-		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/Employee");
-		dataSource.setUsername("newuser");
-		dataSource.setPassword("password");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/Employee");
+        dataSource.setUsername("newuser");
+        dataSource.setPassword("password");
 
-		return dataSource;
+        return dataSource;
 
-	}
+    }
 
-	@Bean
-	@Autowired
-	public LocalSessionFactoryBean getSessionFactory(DataSource dataSource) throws IOException {
+    @Bean
+    @Autowired
+    public LocalSessionFactoryBean getSessionFactory(DataSource dataSource) throws IOException {
 
-		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
-		sessionFactoryBean.setDataSource(dataSource);
-		Properties properties = new Properties();
-		properties.put("hibernate.show_sql", "true");
-		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-		properties.put("hibernate.hbm2ddl.auto", "update");
-		sessionFactoryBean.setAnnotatedClasses(Track.class);
-		sessionFactoryBean.setHibernateProperties(properties);
-		sessionFactoryBean.afterPropertiesSet();
-		return sessionFactoryBean;
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        sessionFactoryBean.setAnnotatedClasses(Track.class);
+        sessionFactoryBean.setHibernateProperties(properties);
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean;
 
-	}
+    }
 
-	@Bean
-	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+    @Bean
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
 
-		HibernateTransactionManager transaction = new HibernateTransactionManager();
-		transaction.setSessionFactory(sessionFactory);
-		return transaction;
-	}
+        HibernateTransactionManager transaction = new HibernateTransactionManager();
+        transaction.setSessionFactory(sessionFactory);
+        return transaction;
+    }
 
 }
